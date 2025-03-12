73241499b94bd5848dff4ab2d70e02845df06503
bobaderanjit007 <bobaderanjit37@gmail.com>
2025-03-12 13:07:42 +0530

    Initial Push

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("nativewind/preset")],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light:{
+          100 : '#D6C6FF',
+          200 : '#A8B5DB',
+          300 : '#9CA4AB',
+        },
+        dark: {
+          100 : '#221f3d',
+          200 : '#0F0d23'
+        },
+        accent: '#AB8BFF',
+      },
+      fontFamily: {
+        sans: ["Inter", "system-ui", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
